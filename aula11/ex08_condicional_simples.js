4c8eea613ea8229e869ccbd29b7115bea365225e
Jason-Brum <contato@jasonb.com.br>
2024-10-07 17:30:51 -0300

    novos exercícios

---
+++ aula11/ex08_condicional_simples.js
@@ -0,0 +1,8 @@
+
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) //condição simples (não tem senão/ else)
+    {
+        console.log(`Você ultrapassou a velocidade máxima permitida: MULTADO`)
+    }
+    console.log(`Dirija sempre com o cinto de segurança!`)
---
+++ aula11/ex09_condicional_composta.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+console.log(`Você nasceu na ${país}. No Brasil você é: `)
+if (país == 'Brasil') 
+{
+    console.log('Brasileiro')
+}
+else {
+    console.log('Estrangeiro')
+}
