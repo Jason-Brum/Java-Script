ddd00b8c0bc469898f17f7c8ee35b188d2805ffc
Jason-Brum <contato@jasonb.com.br>
2024-10-15 14:39:44 -0300

    Mais alguns

---
+++ Digitar_20_numeros/teste.js
@@ -0,0 +1,17 @@
+let numeros = [];
+let soma = 0;
+let media = soma / 20;
+
+let entrada = parseInt(prompt('Digite 20 números de 0 a 100'))
+
+
+for (let e = 0; e < 20; e++) {
+    numeros.push(numero);
+    soma += numero;
+}
+
+
+
+console.log("Números inseridos: ", numeros);
+console.log("Soma: " + soma);
+console.log("Média: " + media);
